09e91e81ac2b6c46aec175cdd48a23cc71301d63
nystromb <bnystrom@siu.edu>
2015-09-13 22:06:23 -0500

    Game ending message and refactored game loop

--- lib/game_runner.rb
+++ lib/game_runner.rb
@@ -1,5 +1,4 @@
 class GameRunner
-  attr_reader :moves
 
   def initialize(output, input)
     @output = output
@@ -28,25 +27,46 @@ class GameRunner
     end
   end
   
+  def game_winner?
+    win_check = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+    win_check.each do |spot|
+      if ((@board.fetch(spot[0]) == "X") && (@board.fetch(spot[1]) == "X") && (@board.fetch(spot[2]) == "X"))
+        return true
+        elsif ((@board.fetch(spot[0]) == "O") && (@board.fetch(spot[1]) == "O") && (@board.fetch(spot[2]) == "O"))
+        return true
+      end
+    end
+    return false
+  end
+  
   def run
     @output.puts "Welcome to Tic Tac Toe"
 
     display_board
     
     turn = 1
-    while !(move = @input.gets).nil?
+    until (move = @input.get_move).nil?
       if turn.odd?
         @board[move.to_i] = "X"
       else
         @board[move.to_i] = "O"
       end
       
-      if (turn += 1) == 9
-        @output.print "Game Over"
+      display_board
+      
+      if (turn += 1) == 9 || game_winner?
+        @output.puts "Game Over"
+        
+        if game_winner? 
+          @output.puts "#{@board.fetch(move.to_i)} wins" 
+        else
+          @output.puts "Draw game"
+        end
+      
+        break
       end
     end
     
-    display_board
   end
   
 end

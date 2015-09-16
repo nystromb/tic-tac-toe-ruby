2b0541f3ca265f768a13c3ff1a4631b618d766f3
nystromb <bnystrom@siu.edu>
2015-09-16 16:59:42 -0500

    Moved set_game_mode & Refactoring
    Made the move_is_valid function positive and extracted set_game_mode
    function from the GameRunner class

--- lib/game_model.rb
+++ lib/game_model.rb
@@ -1,10 +1,22 @@
+require 'tic_tac_toe'
 class Model
+  include TicTacToe
   attr_accessor :mode
-  def initialize
+  def initialize()
     @mode = nil
   end
   
-  def not_valid(move)
-    move < 1 || move > 9
+  def move_is_valid(board, move)
+    (board[move] == EMPTY) && (1..9).include?(move)
+  end
+  
+  def set_game_mode(input, output)
+    output.puts "Please select a game mode\n 1. Human vs Human\n 2. Human vs Computer\n 3. Computer vs Computer\n"
+    mode = input.gets_mode
+    unless (mode.to_i >= 1) && (mode.to_i <= 3)
+      output.puts "Invalid input. Try again"
+      mode = input.gets_mode
+    end
+    self.mode = mode.to_i
   end
 end

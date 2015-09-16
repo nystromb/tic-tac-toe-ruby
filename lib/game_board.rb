51393a8e187fe511071584e64647510753ed25b2
nystromb <bnystrom@siu.edu>
2015-09-16 17:02:56 -0500

    Moved constants into a module

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,6 +1,7 @@
-class Board
+require 'tic_tac_toe'
+class Board  
+  include TicTacToe
   attr_reader :spots
-  EMPTY = "-"
   
   def initialize
     @spots = { 1 => EMPTY, 2 => EMPTY, 3 => EMPTY, 4 => EMPTY, 5 => EMPTY, 6 => EMPTY, 7 => EMPTY, 8 => EMPTY, 9 => EMPTY }
@@ -11,10 +12,10 @@ class Board
   end
   
   def game_winner?
-    winning_combos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-    winning_combos.each do |combo|
+    WINNING_COMBOS.each do |combo|
       return true if ((@spots.fetch(combo[0]) == "X") && (@spots.fetch(combo[1]) == "X") && (@spots.fetch(combo[2]) == "X")) || ((@spots.fetch(combo[0]) == "O") && (@spots.fetch(combo[1]) == "O") && (@spots.fetch(combo[2]) == "O"))
     end
     return false
   end
+  
 end
---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,10 @@
+module TicTacToe
+  X = "X"
+  O = "O"
+  EMPTY = "-"
+  MAX_TURNS = 9
+  WINNING_COMBOS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+  HUMAN_VS_HUMAN = 1
+  HUMAN_VS_COMPUTER = 2
+  COMPUTER_VS_COMPUTER = 3
+end

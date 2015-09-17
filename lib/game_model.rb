bf62dd8f080f517813cfdeaac9b6e284c585e90c
nystromb <bnystrom@siu.edu>
2015-09-17 16:54:59 -0500

    Refactored and added functions to model

--- lib/game_model.rb
+++ lib/game_model.rb
@@ -1,15 +1,44 @@
-require 'tic_tac_toe'
 class Model
-  include TicTacToe
-  attr_accessor :mode
-  def initialize()
+  include GameConstants
+  
+  attr_accessor :mode, :turn
+  def initialize(board = Board.new)
+    @board = board
     @mode = nil
+    @turn = 1
+  end
+
+  def move_is_valid(move)
+    (@board.spots[move] == EMPTY) && (1..9).include?(move)
+  end
+
+  def game_finished?
+    @board.game_winner? || @turn > MAX_TURNS
   end
   
-  def move_is_valid(board, move)
-    (board[move] == EMPTY) && (1..9).include?(move)
+  def display_board(output)
+    @board.spots.each do |spot, contents|
+      output.print contents
+      ([3,6,9].include?(spot)) ? (output.puts) : (output.print "|")
+    end
   end
   
+  def play(move)
+    if @turn.odd?
+      @board.place(move, X)
+    else
+      @board.place(move, O)
+    end
+  end
+
+  def display_winner(output)
+    if @board.game_winner?
+
+    else
+      output.print "Draw game"
+    end
+  end
+
   def set_game_mode(input, output)
     output.puts "Please select a game mode\n 1. Human vs Human\n 2. Human vs Computer\n 3. Computer vs Computer\n"
     mode = input.gets_mode
--- spec/game_model_spec.rb
+++ spec/game_model_spec.rb
@@ -1,31 +1,33 @@
+require 'game_constants'
 require 'game_model'
 require 'game_board'
 
 describe Model do
-  let(:board) { Board.new }
   let(:model) { Model.new }
   
-  it '#move_is_valid returns true for input 1-9' do
-    expect(model.move_is_valid(board.spots, 1)).to eq(true)
+  it '#move_is_valid returns true for input 1' do
+    expect(model.move_is_valid(9)).to eq(true)
   end
   
-  it '#move_is_valid returns true for input 1-9' do
-    expect(model.move_is_valid(board.spots, 9)).to eq(true)
+  it '#move_is_valid returns true for input 9' do
+    expect(model.move_is_valid(1)).to eq(true)
+  end
+  
+  it '#move_is_valid returns true for input 5' do
+    expect(model.move_is_valid(5)).to eq(true)
   end
 
-  it '#move_is_valid returns true for input > 9' do
-    expect(model.move_is_valid(board.spots, 10)).to eq(false)
+  it '#move_is_valid returns false for input > 9' do
+    expect(model.move_is_valid(10)).to eq(false)
   end
   
-  it '#move_is_valid returns true for input < 1' do
-    expect(model.move_is_valid(board.spots, 0)).to eq(false)
+  it '#move_is_valid returns false for input < 1' do
+    expect(model.move_is_valid(0)).to eq(false)
   end
   
   it '#move_is_valid returns false if the spot isn\'t empty' do
-    test_board = board
+    model.play(1)
     
-    test_board.place(1, "X")
-    
-    expect(model.move_is_valid(test_board.spots, 1)).to eq(false)
+    expect(model.move_is_valid(1)).to eq(false)
   end
 end

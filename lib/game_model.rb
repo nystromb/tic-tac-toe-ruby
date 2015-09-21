34fcdd8a6bf22b0d1c1b0a476fc6d5302e998a66
nystromb <bnystrom@siu.edu>
2015-09-21 14:38:52 -0500

    added changes turns and refactored default players
    by default the model will create two regular players, and set the
    player1 to the current_player. The players are only changed if there is
    a computer player needed for a game mode

--- lib/game_model.rb
+++ lib/game_model.rb
@@ -6,21 +6,19 @@ class Model
   def initialize(board = Board.new)
     @board = board
     @turn = 1
+    @player1 = Player.new
+    @player2 = Player.new
+    @current_player = @player1
   end
   
   def init_players(mode)
     case mode
-    when HUMAN_VS_HUMAN
-      @player1 = Player.new
-      @player2 = Player.new
     when HUMAN_VS_COMPUTER
-      @player1 = Player.new
       @player2 = RandomPlayer.new
     when COMPUTER_VS_COMPUTER
       @player1 = RandomPlayer.new
       @player2 = RandomPlayer.new
     end
-    @current_player = @player1
   end
 
   def move_is_valid(move)
@@ -31,8 +29,12 @@ class Model
     (@board.empty_spots.length == 0) || @board.match(WINNING_COMBOS, X) || @board.match(WINNING_COMBOS, O)
   end
   
-  def get_computer_move
-    @current_player.get_move(@board)
+  def get_player_move(input)
+    if @current_player.class == Player
+      @current_player.get_move(input)
+    else
+      @current_player.get_move(@board)
+    end
   end
   
   def play(move)
@@ -40,6 +42,14 @@ class Model
       @board.place(move, X)
     else
       @board.place(move, O)
+    end
+  end
+  
+  def switch_turns
+    if @current_player == @player1
+      @current_player = @player2
+    else
+      @current_player = @player1
     end
   end
   

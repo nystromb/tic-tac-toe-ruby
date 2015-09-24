ffc7fa9d92b653cade488053db93704647e28cf9
nystromb <bnystrom@siu.edu>
2015-09-24 11:35:57 -0500

    Moved displays board and displays winner function to output class

--- lib/game_output.rb
+++ lib/game_output.rb
@@ -1,11 +1,32 @@
-require 'stringio'
-
-class GameOutput < StringIO
+class GameOutput
+  include GameConstants
+  
   def puts(string = "\n")
     $stdout.puts string
   end
   
   def print(string)
     $stdout.print string
+  end
+  
+  def display_board(board)
+    board.spots.each do |spot, contents|
+      print contents
+      if spot % 3 == 0
+        $stdout.puts
+      else
+        $stdout.print "|"
+      end
+    end
+  end
+  
+  def display_winner(board)
+    if board.match(WINNING_COMBOS, X)
+      $stdout.print "X wins\n"
+    elsif board.match(WINNING_COMBOS, O)
+      $stdout.print "O wins\n"
+    else
+      $stdout.print "Draw game\n"
+    end
   end
 end
--- lib/game_runner.rb
+++ lib/game_runner.rb
@@ -7,24 +7,14 @@ class GameRunner
     @input = input
   end
   
-  def get_game_mode(input, output)
-    output.puts "Please select a game mode\n 1. Human vs Human\n 2. Human vs Computer\n 3. Computer vs Computer\n"
-    mode = input.gets_mode
-    unless (mode.to_i >= 1) && (mode.to_i <= 3)
-      output.puts "Invalid input. Try again"
-      mode = input.gets_mode
-    end
-    mode.to_i
-  end
-  
   def run(mode = nil)
-    mode = get_game_mode(@input, @output) if mode.nil?
+    mode = @input.gets_mode(@output) if mode.nil?
     
     @model = Model.new(mode)
     
     @output.print "Welcome to Tic Tac Toe\n"
 
-    @model.display_board(@output)
+    @output.display_board(@model.board)
 
     until @model.is_over?
       if @model.current_player.class == Player
@@ -36,18 +26,16 @@ class GameRunner
       break if move.nil?
 
       if @model.move_is_valid(move.to_i)
-        @output.puts "Played move #{move}"
         @model.play(move.to_i)
       else
         @output.print "Invalid input. Try again\n"
         next
       end
       @model.switch_turns
-      @model.display_board(@output)
+      @output.display_board(@model.board)
     end
 
     @output.print "Game Over\n"
-    @model.display_board(@output)
-    @model.display_winner(@output)
+    @output.display_winner(@model.board)
   end
 end

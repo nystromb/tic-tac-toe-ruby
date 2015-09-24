f54f786163e8a230db019b665bc90b8103ccabc4
nystromb <bnystrom@siu.edu>
2015-09-24 12:12:55 -0500

    Changed MAX_TURNS to MAX_SPOTS

--- lib/game_constants.rb
+++ lib/game_constants.rb
@@ -2,7 +2,7 @@ module GameConstants
   X = "X"
   O = "O"
   EMPTY = "-"
-  MAX_TURNS = 9
+  MAX_SPOTS = 9
   WINNING_COMBOS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
   HUMAN_VS_HUMAN = 1
   HUMAN_VS_COMPUTER = 2

225d3be098cd7d0f77669eeb8d563df4b2ccab01
nystromb <bnystrom@siu.edu>
2015-09-24 12:14:29 -0500

    name changes

--- lib/game_runner.rb
+++ lib/game_runner.rb
@@ -10,32 +10,32 @@ class GameRunner
   def run(mode = nil)
     mode = @input.gets_mode(@output) if mode.nil?
     
-    @model = Model.new(mode)
+    game = Model.new(mode)
     
     @output.print "Welcome to Tic Tac Toe\n"
 
-    @output.display_board(@model.board)
+    @output.display_board(game.board)
 
-    until @model.is_over?
-      if @model.current_player.class == Player
+    until game.is_over?
+      if game.current_player.class == Player
         move = @input.gets_move
       else
-        move = @model.current_player.get_move(@model.clone)
+        move = game.current_player.get_move(game.clone)
       end
   
       break if move.nil?
 
-      if @model.move_is_valid(move.to_i)
-        @model.play(move.to_i)
+      if game.move_is_valid(move.to_i)
+        game.play(move.to_i)
       else
         @output.print "Invalid input. Try again\n"
         next
       end
-      @model.switch_turns
-      @output.display_board(@model.board)
+      game.switch_turns
+      @output.display_board(game.board)
     end
 
     @output.print "Game Over\n"
-    @output.display_winner(@model.board)
+    @output.display_winner(game.board)
   end
 end

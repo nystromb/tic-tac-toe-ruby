394ccd2712468cc8e7cba42da326b438acdc2cd4
nystromb <bnystrom@siu.edu>
2015-09-24 11:36:18 -0500

    Input now gets the mode from the user

--- lib/game_input.rb
+++ lib/game_input.rb
@@ -1,8 +1,12 @@
-require "stringio"
-
-class GameInput < StringIO
-  def gets_mode
-    $stdin.gets
+class GameInput
+  def gets_mode(output)
+    output.puts "Please select a game mode\n 1. Human vs Human\n 2. Human vs Computer\n 3. Computer vs Computer\n"
+    mode = $stdin.gets
+    unless (mode.to_i >= 1) && (mode.to_i <= 3)
+      output.puts "Invalid input. Try again"
+      mode = $stdin.gets_mode
+    end
+    mode.to_i
   end
   
   def gets_move

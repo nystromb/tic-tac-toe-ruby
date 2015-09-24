153207bb9a9ce70f66a52cfd3bcc65132c575b6d
nystromb <bnystrom@siu.edu>
2015-09-24 10:41:00 -0500

    Refactored tests

--- spec/game_runner_spec.rb
+++ spec/game_runner_spec.rb
@@ -37,14 +37,14 @@ describe GameRunner do
   let(:end_game_message) { "Game Over" }
   let(:error_input_message) { "Invalid input. Try again" }
   
-  it 'prints a welcome message' do    
-    runner.run
+  it 'prints a welcome message' do
+    runner.run(1)
     
     expect(output.string).to include(welcome_message)
   end
   
   it 'prints out the board' do
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include(welcome_message + "-|-|-\n")
   end
@@ -52,7 +52,7 @@ describe GameRunner do
   it 'displays the guess 1' do
     input.moves = ["1"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("X|-|-\n")
   end
@@ -60,7 +60,7 @@ describe GameRunner do
   it 'displays the guess 2' do
     input.moves = ["2"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("-|X|-\n")
   end
@@ -68,7 +68,7 @@ describe GameRunner do
   it 'displays the guess 3' do 
     input.moves = ["3"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("-|-|X\n")
   end
@@ -76,7 +76,7 @@ describe GameRunner do
   it 'displays the guess 4' do 
     input.moves = ["4"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("-|-|-\nX|-|-")
   end
@@ -84,7 +84,7 @@ describe GameRunner do
   it 'displays the guess 5' do
     input.moves = ["5"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("-|-|-\n-|X|-")
   end
@@ -92,7 +92,7 @@ describe GameRunner do
   it 'displays the guess 6' do
     input.moves = ["6"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("-|-|-\n-|-|X")
   end
@@ -100,15 +100,15 @@ describe GameRunner do
   it 'displays the guess 7' do
     input.moves = ["7"]
     
-    runner.run
-    
+    runner.run(1)
+
     expect(output.string).to include("-|-|-\n-|-|-\nX|-|-\n")
   end
   
   it 'displays the guess 8' do
     input.moves = ["8"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("-|-|-\n-|-|-\n-|X|-\n")
   end
@@ -116,7 +116,7 @@ describe GameRunner do
   it 'displays the guess 9' do
     input.moves = ["9"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("-|-|-\n-|-|-\n-|-|X\n")
   end
@@ -124,7 +124,7 @@ describe GameRunner do
   it 'displays the guess 1 and 2' do
     input.moves = ["1", "2"]
   
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("X|O|-\n")
   end
@@ -132,7 +132,7 @@ describe GameRunner do
   it 'displays the guess 1 and 3' do
     input.moves =[ "1", "3"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("X|-|O\n")
   end
@@ -140,7 +140,7 @@ describe GameRunner do
   it 'displays the guess 1 and 4' do
     input.moves = ["1", "4"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("X|-|-\nO|-|-\n")
   end
@@ -148,7 +148,7 @@ describe GameRunner do
   it 'displays the guess 1 and 5' do
     input.moves = ["1", "5"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("X|-|-\n-|O|-\n")
   end
@@ -156,7 +156,7 @@ describe GameRunner do
   it 'displays the guess 1 and 6' do
     input.moves = ["1", "6"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("X|-|-\n-|-|O\n")
   end
@@ -164,7 +164,7 @@ describe GameRunner do
   it 'displays the guess 1 and 7' do
     input.moves = ["1", "7"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include("X|-|-\n-|-|-\nO|-|-\n")
   end
@@ -172,7 +172,7 @@ describe GameRunner do
   it 'displays \'Game Over\' when turn is 9' do
     input.moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include(end_game_message)
   end
@@ -180,7 +180,7 @@ describe GameRunner do
   it 'displays \'X Wins\' when x wins' do
     input.moves = ["1", "2", "5", "7", "9"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include(x_wins_message)
   end
@@ -188,7 +188,7 @@ describe GameRunner do
   it 'displays \'O Wins\' when o wins' do
     input.moves = ["1", "5", "3", "2", "4", "8"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include(o_wins_message)
   end
@@ -196,7 +196,7 @@ describe GameRunner do
   it 'displays \'Draw Game\' when its a draw game' do
     input.moves = ["1", "5", "9", "6", "4", "7", "3", "2", "8"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include(draw_game_message)
   end
@@ -204,7 +204,7 @@ describe GameRunner do
   it 'displays an error message on invalid string input' do
     input.moves = ["a string"]  
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include(error_input_message)
   end
@@ -212,7 +212,7 @@ describe GameRunner do
   it 'displays an error message if input is less than 1' do
     input.moves = ["0"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include(error_input_message)
   end
@@ -220,29 +220,7 @@ describe GameRunner do
   it 'displays an error message if input greater than 9' do
     input.moves = ["10"]
     
-    runner.run
-    
-    expect(output.string).to include(error_input_message)
-  end
-  
-  it 'displays a prompt for the game mode if not set' do
-    runner.run
-    
-    expect(output.string).to include("Please select a game mode")
-  end
-  
-  it 'displays error message when input mode is 4' do
-    input.mode = ["4"]
-    
-    runner.run
-    
-    expect(output.string).to include(error_input_message)
-  end
-  
-  it 'displays error message when input mode is 0' do
-    input.mode = ["0"]
-    
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include(error_input_message)
   end
@@ -250,7 +228,7 @@ describe GameRunner do
   it 'doesnt allow the game to make the same move twice' do
     input.moves = ["1", "1"]
     
-    runner.run
+    runner.run(1)
     
     expect(output.string).to include(error_input_message)
   end

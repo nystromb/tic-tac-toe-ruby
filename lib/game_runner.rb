790e55a0d5a00c277c11de71176357f5b95fe894
nystromb <bnystrom@siu.edu>
2015-10-01 09:42:12 -0500

    gets the settings from the user in the initialize class
    I think this should be separated to some kind of settings factory or
    class

--- lib/game_runner.rb
+++ lib/game_runner.rb
@@ -1,42 +1,59 @@
 require './lib/game_model'
 
 class GameRunner
-  
-  def initialize(output, input)
+
+  def initialize(output, input, *args)
     @output = output
     @input = input
+
+    @mode = nil
+    while ((@mode.to_i >= 4) || (@mode.to_i <= 0))
+      @output.puts "Please select a game mode\n 1. Human vs Human\n 2. Human vs Computer\n 3. Computer vs Computer\n"
+      @mode = @input.gets.to_i
+    end
+    @output.puts "Mode: Human vs. Human" if @mode == 1
+    @output.puts "Mode: Human vs. Computer" if @mode == 2
+    @output.puts "Mode: Computer vs. Computer" if @mode == 3
+
+    @output.print "Do you want to play on a 4x4 board? y/n\n"
+    @board_size = @input.gets
+    if (@board_size == "y" || @board_size == "y\n")
+      @board_size = 4
+    else
+      @board_size = 3
+    end
+
+    @output.print "The board size is #{@board_size}x#{@board_size}\n"
+
+    @game = Model.new(@mode, @board_size)
   end
-  
-  def run(mode = nil)
-    mode = @input.gets_mode(@output) if mode.nil?
-    
-    game = Model.new(mode)
-    
+
+  def run
     @output.print "Welcome to Tic Tac Toe\n"
 
-    @output.display_board(game.board)
+    @output.display_board(@game.board)
 
-    until game.is_over?
-      if game.current_player.class == Player
-        move = @input.gets_move
+    until @game.is_over?
+      if @game.current_player.class == Player
+        move = @input.gets
       else
-        move = game.current_player.get_move(game.clone)
+        move = @game.current_player.get_move(@game.clone)
       end
-  
+
       break if move.nil?
 
-      if game.move_is_valid(move.to_i)
-        game.play(move.to_i)
+      if @game.move_is_valid(move.to_i)
+        @game.play(move.to_i)
       else
         @output.print "Invalid input. Try again\n"
         next
       end
-  
-      game.switch_turns
-      @output.display_board(game.board)
+
+      @game.switch_turns
+      @output.display_board(@game.board)
     end
 
     @output.print "Game Over\n"
-    @output.display_winner(game.board)
+    @output.display_winner(@game.board)
   end
 end

dc0eb34aecd69c258b3ab1b4ab21cf91fa3d38d6
nystromb <bnystrom@siu.edu>
2015-10-01 09:39:38 -0500

    took out the game mode prompt from the input class

--- lib/game_input.rb
+++ lib/game_input.rb
@@ -1,15 +1,5 @@
 class GameInput
-  def gets_mode(output)
-    output.puts "Please select a game mode\n 1. Human vs Human\n 2. Human vs Computer\n 3. Computer vs Computer\n"
-    mode = $stdin.gets
-    unless (mode.to_i >= 1) && (mode.to_i <= 3)
-      output.puts "Invalid input. Try again"
-      mode = $stdin.gets_mode
-    end
-    mode.to_i
-  end
-  
-  def gets_move
+  def gets
     $stdin.gets
   end
 end

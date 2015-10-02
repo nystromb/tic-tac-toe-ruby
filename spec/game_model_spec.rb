c3fdd75aa2b8f0c78213380609b7e9c4fceaab13
nystromb <bnystrom@siu.edu>
2015-10-02 14:20:41 -0500

    cleaned up game model tests

--- spec/game_model_spec.rb
+++ spec/game_model_spec.rb
@@ -1,7 +1,8 @@
 require 'game_model'
 
 describe Model do
-  let(:model) { Model.new(1, 3)}
+  let(:board) { Board.new }
+  let(:model) { Model.new(board, GameModes::HUMAN_VS_HUMAN)}
 
   it '#move_is_valid returns true for input 1' do
     expect(model.move_is_valid(1)).to eq(true)
@@ -29,101 +30,71 @@ describe Model do
     expect(model.move_is_valid(1)).to eq(false)
   end
 
-  it 'game is complete if the board is full' do
-    test_model = model
-
+  it 'game is complete if the board is full' do 
     9.times do |spot|
-      test_model.play(spot)
+      model.play(spot)
     end
 
-    expect(test_model.is_over?).to eq(true)
+    expect(model.is_over?).to eq(true)
   end
 
   it 'game is complete if X has a winning combination' do
-    test_model = model
+    model.play(1) #X
+    model.play(2) #X
+    model.play(3) #X
 
-    test_model.play(1) #X
-    test_model.play(2) #X
-    test_model.play(3) #X
-
-    expect(test_model.is_over?).to eq(true)
+    expect(model.is_over?).to eq(true)
   end
 
   it 'game is complete if O has a winning combination' do
-    test_model = model
+    model.switch_turns
 
-    test_model.switch_turns
+    model.play(4) #O
+    model.play(5) #O
+    model.play(6) #O
 
-    test_model.play(4) #O
-    test_model.play(5) #O
-    test_model.play(6) #O
-
-    expect(test_model.is_over?).to eq(true)
+    expect(model.is_over?).to eq(true)
   end
 
   it 'player1 is of the Player class when mode is Human vs Human' do
-    test_model = model
-
-    expect(test_model.players[1].class).to eq(Player)
+    expect(model.players[1].class).to eq(Player)
   end
 
   it 'player2 is of the Player class when mode is Human vs Human' do
-    test_model = model
-
-    expect(test_model.players[2].class).to eq(Player)
+    expect(model.players[2].class).to eq(Player)
   end
 
   it 'player1 is a Player when game mode is Human vs Computer' do
-    test_model = Model.new(2, 3)
+    model = Model.new(board, GameModes::HUMAN_VS_COMPUTER)
 
-    expect(test_model.players[1].class).to eq(Player)
+    expect(model.players[1].class).to eq(Player)
   end
 
   it 'player2 is a RandomPlayer when game mode is Human vs Computer' do
-    test_model = Model.new(2, 3)
+    model = Model.new(board, GameModes::HUMAN_VS_COMPUTER)
 
-    expect(test_model.players[2].class).to eq(UnbeatablePlayer)
+    expect(model.players[2].class).to eq(UnbeatablePlayer)
   end
 
   it 'player1 is a RandomPlayer when the game mode is Computer vs Computer' do
-    test_model = Model.new(3, 3)
+    model = Model.new(board, GameModes::COMPUTER_VS_COMPUTER)
 
     expect(test_model.players[1].class).to eq(UnbeatablePlayer)
   end
 
   it 'player2 is a RandomPlayer when the game mode is Computer vs Computer' do
-    test_model = Model.new(3, 3)
+    test_model = Model.new(board, GameModes::COMPUTER_VS_COMPUTER)
 
     expect(test_model.players[2].class).to eq(UnbeatablePlayer)
   end
 
   it 'switches current_player from player1 turn to player2' do
-    test_model = model
-
-    expect(test_model.current_player).to eq(test_model.players[1])
-
-    test_model.switch_turns
-
-    expect(test_model.current_player).to eq(test_model.players[2])
+    expect(model.current_player).to eq(model.players[1])
   end
+  
+  it 'switches player1 to player2' do
+    model.switch_turns
 
-  it 'switches current_player from player2 turn to player1 turn' do
-    test_model = model
-
-    test_model.switch_turns
-
-    expect(test_model.current_player).to eq(test_model.players[2])
-
-    test_model.switch_turns
-
-    expect(test_model.current_player).to eq(test_model.players[1])
-  end
-
-  it 'sets default player' do
-    test_model = model
-
-    test_model.set_current_player
-
-    expect(test_model.current_player).to eq(test_model.players[1])
+    expect(model.current_player).to eq(model.players[2])
   end
 end

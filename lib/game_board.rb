e9a42fe3a6ecef94929729d3b72bfe79fdaa4fa2
nystromb <bnystrom@siu.edu>
2015-10-02 14:04:35 -0500

    Cleaned up tests and board default size is 3

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -4,12 +4,9 @@ class Board < Hash
   include GamePieces
   attr_accessor :size
   
-  def initialize(size)
-    @win_rules = {
-      3 => [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]], 
-      4 => [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16], [1,5,9,13], [2,6,10,14], [3,7,11,15], [4,8,12,16],[1,6,11,16],[4,7,10,13]]
-      }
-    @size = size
+  WIN_INDEXES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+  def initialize
+    @size = 3
     clear
   end
 
@@ -34,12 +31,8 @@ class Board < Hash
   end
 
   def win(game_peice)
-    @win_rules[@size].each do |index|
-      if @size == 4
-        return true if ((self.fetch(index[0]) == game_peice) && (self.fetch(index[1]) == game_peice) && (self.fetch(index[2]) == game_peice) && (self.fetch(index[3]) == game_peice))
-      elsif @size == 3
-        return true if ((self.fetch(index[0]) == game_peice) && (self.fetch(index[1]) == game_peice) && (self.fetch(index[2]) == game_peice))
-      end
+    WIN_INDEXES.each do |index|
+      return true if (self.fetch(index[0]) == game_peice) && (self.fetch(index[1]) == game_peice) && (self.fetch(index[2]) == game_peice)
     end
     return false
   end
--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -1,117 +1,41 @@
-require './lib/game_board'
+require_relative '../lib/game_board'
 
 describe Board do
-  let(:board) { Board.new(3) }
-
-  it 'board has a size' do
+  let(:board) { Board.new }
+  
+  it 'has a board size of 3' do
     expect(board.size).to eq(3)
   end
-
-  it 'can have a board size of 4' do
-    new_board = Board.new(4)
-
-    expect(new_board.size).to eq(4)
+  
+  it 'has a cell count of 9' do
+    expect(board.cell_count).to eq(9)
   end
-
-  it 'can tell you the number of cells' do
-    new_board = Board.new(4)
-    expect(new_board.cell_count).to eq(16)
+  
+  it 'win is false if no moves have been played' do
+    expect(board.win(GamePieces::X)).to be_falsey
   end
-
-  it 'clears each spot to EMPTY' do
-    expect(board.fetch(1)).to eq(GamePieces::EMPTY)
+  
+  it 'win is true if three Pieces in a row at 1, 2, 3' do
+    board.place(1, GamePieces::X)
+    board.place(2, GamePieces::X)
+    board.place(3, GamePieces::X)
+    
+    expect(board.win(GamePieces::X)).to be_truthy
   end
-
-  it 'clears each spot to EMPTY' do
-    expect(board.fetch(9)).to eq(GamePieces::EMPTY)
+  
+  it 'win is true if three Pieces in a row at 4, 5, 6' do
+    board.place(4, GamePieces::X)
+    board.place(5, GamePieces::X)
+    board.place(6, GamePieces::X)
+    
+    expect(board.win(GamePieces::X)).to be_truthy
   end
-
-  it 'can place moves on the board' do
-    test_board = board
-
-    test_board.place(1, "X")
-
-    expect(board.fetch(1)).to eq("X")
-  end
-
-  it 'returns the indexes of the empty board spaces' do
-    test_board = board
-
-    test_board.place(1, "X")
-    test_board.place(2, "X")
-    test_board.place(3, "X")
-
-    expect(test_board.empty_spots).to eq([4,5,6,7,8,9])
-  end
-
-  it 'determines the win based on the board size' do
-    test_board = Board.new(4)
-
-    test_board.place(1, "X")
-    test_board.place(2, "X")
-    test_board.place(3, "X")
-    test_board.place(4, "X")
-
-    expect(test_board.win(GamePieces::X)).to be_truthy
-  end
-
-  it 'matches a set of game winning indexes to a game peice' do
-    expect(board.win(GamePieces::X)).to eq(false)
-  end
-
-  it 'matches if X is at spots 1, 2, and 3' do
-    test_board = board
-
-    test_board.place(1, "X")
-    test_board.place(2, "X")
-    test_board.place(3, "X")
-
-    expect(test_board.win(GamePieces::X)).to eq(true)
-  end
-
-  it 'matches if X is at spots 3, 5, and 7' do
-    test_board = board
-
-    test_board.place(3, GamePieces::X)
-    test_board.place(5, GamePieces::X)
-    test_board.place(7, GamePieces::X)
-
-    expect(test_board.win(GamePieces::X)).to eq(true)
-  end
-
-  it 'matches if O is at spots 1, 2, and 3' do
-    test_board = board
-
-    test_board.place(1, GamePieces::O)
-    test_board.place(2, GamePieces::O)
-    test_board.place(3, GamePieces::O)
-
-    expect(test_board.win(GamePieces::O)).to eq(true)
-  end
-
-  it 'matches if O is at spots 1, 4, and 7' do
-    test_board = board
-
-    test_board.place(1, GamePieces::O)
-    test_board.place(4, GamePieces::O)
-    test_board.place(7, GamePieces::O)
-
-    expect(test_board.win(GamePieces::O)).to eq(true)
-  end
-
-  it 'can return the empty game board locations, if any' do
-    test_board = board
-
-    test_board.place(1, GamePieces::O)
-    test_board.place(2, GamePieces::O)
-    test_board.place(3, GamePieces::O)
-    test_board.place(4, GamePieces::O)
-    test_board.place(5, GamePieces::O)
-    test_board.place(6, GamePieces::O)
-    test_board.place(7, GamePieces::O)
-    test_board.place(8, GamePieces::O)
-    test_board.place(9, GamePieces::O)
-
-    expect(test_board.empty_spots.length).to eq(0)
+  
+  it 'win is true if three Pieces in a row at 7, 8, 9' do
+    board.place(4, GamePieces::O)
+    board.place(5, GamePieces::O)
+    board.place(6, GamePieces::O)
+    
+    expect(board.win(GamePieces::O)).to be_truthy
   end
 end

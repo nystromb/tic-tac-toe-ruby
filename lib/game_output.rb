7a6a01483b9785ca5eb2763ce77e38a391a68bae
nystromb <bnystrom@siu.edu>
2015-10-02 15:50:06 -0500

    Made output a StringIO to take out unnecessary methods

--- lib/game_output.rb
+++ lib/game_output.rb
@@ -1,32 +1,26 @@
-class GameOutput
+require 'stringio'
+
+class GameOutput < StringIO
   include GamePieces
-
-  def puts(string = "\n")
-    $stdout.puts string
-  end
-
-  def print(string)
-    $stdout.print string
-  end
 
   def display_board(board)
     board.each do |spot, contents|
-      $stdout.print contents
+      self.print contents
       if (spot % board.size) == 0
-        $stdout.puts
+        self.puts
       else
-        $stdout.print "|"
+        self.print "|"
       end
     end
   end
 
   def display_winner(board)
     if board.win(X)
-      $stdout.print "X wins\n"
+      self.print "X wins\n"
     elsif board.win(O)
-      $stdout.print "O wins\n"
+      self.print "O wins\n"
     else
-      $stdout.print "Draw game\n"
+      self.print "Draw game\n"
     end
   end
 end

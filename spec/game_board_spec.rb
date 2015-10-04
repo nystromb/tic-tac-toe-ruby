078c16b2cc4e0f06a15621c7e5b7cf585456b670
nystromb <bnystrom@siu.edu>
2015-10-04 01:49:47 -0500

    Calls a 3x3 board for test cases

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -1,14 +1,15 @@
 require_relative '../lib/game_board'
+require_relative '../lib/game_three_by_three_board'
 
 describe Board do
-  let(:board) { Board.new }
+  let(:board) { ThreeByThreeBoard.new }
   
-  it 'has a board size of 1 by default' do
-    expect(board.size).to eq(1)
+  it 'has a board size of 3 by default' do
+    expect(board.size).to eq(3)
   end
   
-  it 'has a cell count of 1^2 == 1' do
-    expect(board.cell_count).to eq(1)
+  it 'has a cell count of the size squared' do
+    expect(board.cell_count).to eq(9)
   end
   
   it 'initializes the board spots to EMPTY' do
@@ -16,15 +17,15 @@ describe Board do
   end
   
   it 'can return the empty spots on the board' do
-    expect(board.empty_spots).to eq([1])
+    expect(board.empty_spots).to eq([1,2,3,4,5,6,7,8,9])
   end
   
   it 'can get the amount of empty spots' do
-    expect(board.empty_spots.length).to eq(1)
+    expect(board.empty_spots.length).to eq(9)
   end
   
   it 'can tell you if the board is empty' do
     board.place(1, GamePieces::X)
-    expect(board.empty_spots.length).to eq(0)
+    expect(board.empty_spots.length).to eq(8)
   end
 end

69b890791ebe838e5106f92fa3b1db981676133c
nystromb <bnystrom@siu.edu>
2015-10-05 13:23:18 -0500

    cleaned up init and creates players

--- lib/game_runner.rb
+++ lib/game_runner.rb
@@ -3,19 +3,19 @@ require './lib/game_three_by_three_board.rb'
 require './lib/game_four_by_four_board.rb'
 
 class GameRunner
+  include PlayerFactory
 
   def initialize(output, input, *args)
     @output = output
     @input = input
-
-    @mode = nil
-    while ((@mode.to_i >= 4) || (@mode.to_i <= 0))
-      @output.puts "Please select a game mode\n 1. Human vs Human\n 2. Human vs Computer\n 3. Computer vs Computer\n"
-      @mode = @input.gets.to_i
-    end
-    @output.puts "Mode: Human vs. Human" if @mode == 1
-    @output.puts "Mode: Human vs. Computer" if @mode == 2
-    @output.puts "Mode: Computer vs. Computer" if @mode == 3
+    
+    mode = get_game_mode
+    
+    @output.puts "Mode: Human vs. Human" if mode == 1
+    @output.puts "Mode: Human vs. Computer" if mode == 2
+    @output.puts "Mode: Computer vs. Computer" if mode == 3
+    
+    @players = PlayerFactory.createPlayers(mode)
 
     @output.print "Do you want to play on a 4x4 board? y/n\n"
     @board_size = @input.gets
@@ -27,7 +27,16 @@ class GameRunner
 
     @output.print "The board size is #{@board.size}x#{@board.size}\n"
 
-    @game = Model.new(@board, @mode)
+    @game = Model.new(@board, @players)
+  end
+
+  def get_game_mode
+    mode = nil
+    while ((mode.to_i >= 4) || (mode.to_i <= 0))
+      @output.puts "Please select a game mode\n 1. Human vs Human\n 2. Human vs Computer\n 3. Computer vs Computer\n"
+      mode = @input.gets.to_i
+    end
+    mode 
   end
 
   def run

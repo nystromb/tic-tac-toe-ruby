15aaa1d4efc4134df7664b14916806b12f9d61d8
nystromb <bnystrom@siu.edu>
2015-10-04 19:54:46 -0500

    win_lineup returns result of winning conditional

--- lib/game_three_by_three_board.rb
+++ lib/game_three_by_three_board.rb
@@ -10,6 +10,10 @@ class ThreeByThreeBoard < Board
     3
   end
   
+  def win_lineup(index, game_piece)
+    (self.fetch(index[0]) == game_piece) && (self.fetch(index[1]) == game_piece) && (self.fetch(index[2]) == game_piece)  
+  end
+  
   def win_indexes
     [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
   end

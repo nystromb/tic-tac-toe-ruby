1eee29aa03954e4f607f1e0296d3258a2587e4cd
nystromb <bnystrom@siu.edu>
2015-10-04 19:24:26 -0500

    4x4 Board class

---
+++ lib/game_four_by_four_board.rb
@@ -0,0 +1,17 @@
+require './lib/game_board'
+
+class FourByFourBoard < Board
+  
+  def initialize()
+    super
+  end
+  
+  def size
+    4
+  end
+  
+  def win_indexes
+    [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16],[1,5,9,13],[2,6,10,14],[3,7,11,15],[4,8,12,16],[1,6,11,16],[4,7,10,13]]
+  end
+  
+end

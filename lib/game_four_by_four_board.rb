b3bb1d2265d74d9ca04f01ca0e2abe4d1406b0fc
nystromb <bnystrom@siu.edu>
2015-10-04 19:55:41 -0500

    implemented win_lineup 4x4

--- lib/game_four_by_four_board.rb
+++ lib/game_four_by_four_board.rb
@@ -10,6 +10,10 @@ class FourByFourBoard < Board
     4
   end
   
+  def win_lineup(index, game_piece)
+    (self.fetch(index[0]) == game_piece) && (self.fetch(index[1]) == game_piece) && (self.fetch(index[2]) == game_piece)  
+  end
+  
   def win_indexes
     [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16],[1,5,9,13],[2,6,10,14],[3,7,11,15],[4,8,12,16],[1,6,11,16],[4,7,10,13]]
   end
